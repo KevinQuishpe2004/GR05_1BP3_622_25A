71269594c8e9dd432e2f7c7cf0e266dbdfb6d403
DANIEL FERNANDO MERA CAMPOVERDE <daniel.mera01@gmail.com>
2025-05-11 19:00:02 -0500

    Creación de la clase CalculatorExecute y el empaquetado .jar respectivo.

---
+++ src/main/java/ec/epn/edu/CalculatorExecute.java
@@ -0,0 +1,14 @@
+package ec.epn.edu;
+
+
+public class CalculatorExecute {
+    public static void main(String[] args) {
+        System.out.println("EJECUTANDO LA CALCULADORA!!!!!!!!!!!");
+        System.out.println("la suma entre 10 y 20 es " + Calculator.add(10,20) );
+        System.out.println("la resta entre 10 y 20 es " + Calculator.subtract(10,20) );
+        System.out.println("la multiplicacion entre 10 + 20 es " + Calculator.multiply(10,20));
+        System.out.println("la división entre 20 y 10 es " + Calculator.add(20,10) );
+        System.out.println("la sumatoria de 100 es " + Calculator.summation(100) );
+    }
+}
+
